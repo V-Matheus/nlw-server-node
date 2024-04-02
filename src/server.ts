dae10c9b341772a7a883bd5443a91ad24e18a90b
Victor Matheus <victormatheus507@gmail.com>
2024-04-02 18:41:10 -0300

    mandando informacao para o banco de dados

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,12 @@
 import fastify from 'fastify';
+import { z } from 'zod';
+import { PrismaClient } from '@prisma/client';
 
 const app = fastify();
+
+const prisma = new PrismaClient({
+  log: ['query'],
+});
 
 // Métodos HTTP: GET, POST, PUT, DELETE, PATCH, HEAD,OPTIONS, ...
 
@@ -11,10 +17,26 @@ const app = fastify();
 
 // SOAP, REST
 
-app.post('/events', (request, reply) => {
-  console.log(request.body);
+app.post('/events', async (request, reply) => {
+  const createEventSchema = z.object({
+    title: z.string().min(4),
+    details: z.string().nullable(),
+    maximumAttendees: z.number().int().positive().nullable(),
+  });
 
-  return 'Hello NLW Unite';
+  const data = createEventSchema.parse(request.body);
+
+  const event = await prisma.event.create({
+    data: {
+      title: data.title,
+      details: data.details,
+      maximumAttendees: data.maximumAttendees,
+      slug: new Date().toISOString(),
+    },
+  });
+
+  // return { eventId: event.id };
+  return reply.status(201).send({ eventId: event.id });
 });
 
 app.listen({ port: 3333 }).then(() => {
