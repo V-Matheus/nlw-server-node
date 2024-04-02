d11890f108bbcd90b031132f8ff980ea924c3535
Victor Matheus <victormatheus507@gmail.com>
2024-04-02 18:18:25 -0300

    criando banco de dados com o prisma

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,19 @@ import fastify from 'fastify';
 
 const app = fastify();
 
+// Métodos HTTP: GET, POST, PUT, DELETE, PATCH, HEAD,OPTIONS, ...
+
+// Corpo de requisiçã (request body)
+// Parâmetros de busca (Search Params / Query Params)
+// Parâmetros de rotas
+// Cabeçalhos
+
+// SOAP, REST
+
+app.get('/', () => {
+  return 'Hello NLW Unite';
+});
+
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP Server Running');
 });
